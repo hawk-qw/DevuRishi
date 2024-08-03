3c77362cd1ca3d15f39d6ecbf2da19409fa928ef
hawk-qw <rishisr.4409@gmail.com>
2024-08-03 21:30:11 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
+
+  base: 'DevuRishi',
   plugins: [react()],
-})
+
+});
+
+
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
